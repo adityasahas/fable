24532ae9e65281f397d3f80f082520e2e96ccafe
Aditya Sahasranam <adityasahas2025@gmail.com>
2025-01-28 19:06:14 -0800

    Fix dbus-daemon startup by creating socket directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-dbus-daemon --system --fork --nopidfile
+mkdir -p /var/run/dbus
+dbus-daemon --system --fork --address=unix:path=/var/run/dbus/system_bus_socket
 
 source /opt/conda/etc/profile.d/conda.sh
 conda activate fable_env
-
 
 python -m uvicorn main:app --host 0.0.0.0 --port $PORT
